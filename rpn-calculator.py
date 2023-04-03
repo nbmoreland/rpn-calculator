190968d9a4327224ad035c29be1c4dda8cc002f2
nicholasmoreland <nmoreland18@outlook.com>
2023-04-03 13:34:49 -0500

    feat: add calculate_result function for expression

--- rpn-calculator.py
+++ rpn-calculator.py
@@ -7,3 +7,18 @@ OPERANDS = {
     "*": lambda a, b: a * b,
     "/": lambda a, b: a / b
 }
+
+# Calculate our result for the given RPN expression
+def calculate_result( expression ):
+    stack = []
+
+    for key in expression:
+        if key in OPERANDS:
+            b = stack.pop()
+            a = stack.pop()
+
+            result = OPERANDS[key](a, b)
+            stack.append(result)
+        else:
+            stack.append( int( key ))
+    return stack.pop()
