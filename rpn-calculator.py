a582f9211634979822cc1686279f8e902c98c9fa
nicholasmoreland <nmoreland18@outlook.com>
2023-04-03 13:40:54 -0500

    update: loop through text file for rpn expressions

--- rpn-calculator.py
+++ rpn-calculator.py
@@ -22,3 +22,11 @@ def calculate_result( expression ):
         else:
             stack.append( int( key ))
     return stack.pop()
+
+with open( os.path.join( os.getcwd(), "input_RPN.txt"), "r" ) as file:
+    for line in file:
+        line = line.rstrip("\n")
+        expression = line.split()
+
+        result = calculate_result( expression )
+        print( result )
